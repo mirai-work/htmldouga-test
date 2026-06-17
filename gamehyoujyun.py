b50185232ff29b16c1a128f1983fe645ff2e9d47
mirai-work <mfkensub@gmail.com>
2026-06-17 11:07:48 +0900

    Update gamehyoujyun.py

--- gamehyoujyun.py
+++ gamehyoujyun.py
@@ -1,56 +1,50 @@
 import pyxel
 import js
-from pyodide.ffi import create_proxy # ← これが必要です！
 
 class App:
 
-    def __init__(self):
-        pyxel.init(160, 120)
-        self.state = "TITLE"
-        pyxel.run(
-            self.update,
-            self.draw
-        )
 
-    def play_movie(self, filename):
-        self.state = "MOVIE"
-        
-        # JavaScriptに安全にPythonの関数を渡すための「プロキシ」を作成
-        # これによりJavaScript側から実行されても self を見失わなくなります
-        js_callback = create_proxy(self.movie_finished)
+def __init__(self):
 
-        # 文字列ではなく、作成したプロキシを直接渡す
-        js.playMovie(filename, js_callback)
+    pyxel.init(160, 120)
 
-    def movie_finished(self):
-        print("動画終了")
-        self.state = "TITLE"
+    self.state = "TITLE"
 
-    def update(self):
-        if self.state == "TITLE":
-            if pyxel.btnp(pyxel.KEY_1):
-                self.play_movie("opening.mp4")
+    js.window.pyxel_app = self
 
-            if pyxel.btnp(pyxel.KEY_2):
-                self.play_movie("ending.mp4")
+    pyxel.run(self.update, self.draw)
 
-            if pyxel.btnp(pyxel.KEY_3):
-                self.play_movie("sonota.mp4")
+def show_photo(self):
 
-    def draw(self):
-        pyxel.cls(0)
+    self.state = "PHOTO"
 
-        if self.state == "TITLE":
-            pyxel.text(20, 20, "1 : OPENING", 7)
-            pyxel.text(20, 40, "2 : ENDING", 7)
-            pyxel.text(20, 60, "3 : SONOTA MOVIE", 7)
+    js.showImage(
+        "photo.PNG",
+        "photo_finished"
+    )
 
-        elif self.state == "MOVIE":
-            pyxel.text(
-                35,
-                60,
-                "MOVIE PLAYING...",
-                8
-            )
+def photo_finished(self):
+
+    self.state = "TITLE"
+
+def update(self):
+
+    if self.state == "TITLE":
+
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.show_photo()
+
+def draw(self):
+
+    pyxel.cls(0)
+
+    pyxel.text(
+        20,
+        50,
+        "SPACE : PHOTO",
+        7
+    )
+
 
 App()
+
