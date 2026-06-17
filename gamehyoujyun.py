533c516693d223d12bd7e4a83f499aeaaf5daf21
mirai-work <mfkensub@gmail.com>
2026-06-17 11:33:13 +0900

    Update gamehyoujyun.py

--- gamehyoujyun.py
+++ gamehyoujyun.py
@@ -6,19 +6,14 @@ class App:
     def __init__(self):
         pyxel.init(160, 120)
         self.state = "TITLE"
-        
-        # JavaScript側からPythonのメソッドを呼べるように登録
         js.window.pyxel_app = self
-
         pyxel.run(self.update, self.draw)
 
     def show_photo(self):
         self.state = "PHOTO"
-        # 画像ファイル名だけを渡してJSの関数を呼び出す
         js.showImage("photo.PNG")
 
     def photo_finished(self):
-        # JS側から画像が閉じられたら、ステートをTITLEに戻す
         self.state = "TITLE"
 
     def update(self):
@@ -28,10 +23,10 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        
         if self.state == "TITLE":
             pyxel.text(20, 50, "SPACE : PHOTO", 7)
         elif self.state == "PHOTO":
             pyxel.text(35, 50, "SHOWING PHOTO...", 8)
 
+# 【注意】App() は class の外側（左端に寄せる）で呼び出します
 App()
